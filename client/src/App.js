bf0e27700eddf76fbdafaa9c100b3c04bdced49c
Justin Cheung <justin.cheung08@gmail.com>
2020-11-25 15:50:33 -0800

    reformated folder structure and moved switches to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,31 @@
 import React from "react"
 import "./style/App.css"
-import WarehousePage from "./components/WarehousePage"
+import { Switch, Route } from "react-router-dom"
+import Header from "./components/Header"
+import WarehouseDetails from "./Pages/WarehouseDetails"
+import EditWarehouse from "./Pages/EditWarehouse"
+import AddWarehouse from "./Pages/AddWarehouse"
+import WarehousePage from "./Pages/WarehousesPage"
 
 class App extends React.Component {
-  state = {
-    warehouses: [
-      {
-        id: "2922c286-16cd-4d43-ab98-c79f698aeab0",
-        name: "Manhattan",
-        address: "503 Broadway",
-        city: "New York",
-        country: "USA",
-        contact: {
-          name: "Parmin Aujla",
-          position: "Warehouse Manager",
-          phone: "+1 (646) 123-1234",
-          email: "paujla@instock.com",
-        },
-      },
-      {
-        id: "5bf7bd6c-2b16-4129-bddc-9d37ff8539e9",
-        name: "King West",
-        address: "469 King Street West",
-        city: "Toronto",
-        country: "CAN",
-        contact: {
-          name: "Greame Lyon",
-          position: "Warehouse Manager",
-          phone: "+1 (646) 123-1234",
-          email: "glyon@instock.com",
-        },
-      },
-    ],
-    inventories: [],
-  }
-
   render() {
     return (
-      <>
-        <WarehousePage warehouseList={this.state.warehouses} />
-      </>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={WarehousePage} />
+          <Route
+            path="/warehouse/:warehouseID"
+            exact
+            component={WarehouseDetails}
+          />
+          <Route
+            path="/warehouse/:warehouseID/edit"
+            component={EditWarehouse}
+          />
+          <Route path="/warehouse/add" exact component={AddWarehouse} />
+        </Switch>
+      </div>
     )
   }
 }
