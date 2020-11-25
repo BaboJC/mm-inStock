ba1cb996c5fdb0b10499272f282def88e45b4794
Justin Cheung <justin.cheung08@gmail.com>
2020-11-24 16:36:55 -0800

    added more necessary assets and folder structure

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+function Navigation() {
+  return (
+    <ul className="navigation">
+      <li className="navigation__item">Warehouses</li>
+      <li className="navigation__item">Inventory</li>
+    </ul>
+  )
+}
+
+export default Navigation
