2c8f26fa986d86edd7f531a177456ca91db0935f
gus-santos <72359205+gus-santos@users.noreply.github.com>
2020-11-25 17:14:02 -0800

    Revert "Get all inventory items"

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,17 @@
 import React from "react"
 import "./style/App.css"
-import WarehousePage from "./components/WarehousePage"
 
 class App extends React.Component {
   state = {
-    warehouses: [
-      {
-        id: "2922c286-16cd-4d43-ab98-c79f698aeab0",
-        name: "Manhattan",
-        address: "503 Broadway",
-        city: "New York",
-        country: "USA",
-        contact: {
-          name: "Parmin Aujla",
-          position: "Warehouse Manager",
-          phone: "+1 (646) 123-1234",
-          email: "paujla@instock.com",
-        },
-      },
-      {
-        id: "5bf7bd6c-2b16-4129-bddc-9d37ff8539e9",
-        name: "King West",
-        address: "469 King Street West",
-        city: "Toronto",
-        country: "CAN",
-        contact: {
-          name: "Greame Lyon",
-          position: "Warehouse Manager",
-          phone: "+1 (646) 123-1234",
-          email: "glyon@instock.com",
-        },
-      },
-    ],
+    warehouses: [],
     inventories: [],
   }
 
   render() {
     return (
-      <>
-        <WarehousePage warehouseList={this.state.warehouses} />
-      </>
+      <div>
+        <h1>InStock</h1>
+      </div>
     )
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,11 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import Header from "./components/Header"
-import WarehouseDetails from "./components/WarehouseDetails"
-import EditWarehouse from "./components/EditWarehouse"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import AddWarehouse from "./components/AddWarehouse"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={App} />
-        <Route
-          path="/warehouse/:warehouseID"
-          exact
-          component={WarehouseDetails}
-        />
-        <Route path="/warehouse/:warehouseID/edit" component={EditWarehouse} />
-        <Route path="/warehouse/add" exact component={AddWarehouse} />
-      </Switch>
-    </Router>
+    <Header />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 )
